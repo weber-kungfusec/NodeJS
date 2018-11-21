577c9f073c859c4d9846248725b0d2cf87b5d736
WeberRess <weber.ress@ibm.com>
2018-11-21 13:11:40 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const http = require('http');
+var http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var server = http.createServer(function(request, response) {
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World Full DevOps !!\n');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
 
+console.log("Server running at http://localhost:%d", port);
