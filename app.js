7ab866d74989ebda8b531de098313e9dfeef8548
WeberRess <weber.ress@ibm.com>
2018-12-19 10:31:31 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("CloudSec team - line 1 !!!");
-    response.end("CloudSec team - line 2 !!!");
-
+    response.end("CloudSec team - column 1 !!! -- CloudSec team - column 2 !!!");
+    
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+}
+
