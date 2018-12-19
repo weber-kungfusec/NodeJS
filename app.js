d7a7ec714c4ad3d5f274a11f952efc83f829a2fa
WeberRess <weber.ress@ibm.com>
2018-12-19 10:37:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("CloudSec team - column 1 !!! -- CloudSec team - column 2 !!!");
+    response.end("CloudSec team - column 1 !");
     
 });
 
