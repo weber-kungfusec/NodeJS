01e500158a61f15e11eb28ed89b80383ac00c1d4
WeberRess <weber.ress@ibm.com>
2018-12-19 11:00:57 +0000

    App updated v2
    ssss

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World CloudSec team !");
+    response.end("Hello World CloudSec team **");
 });
 var port = process.env.PORT || 1337;
 server.listen(port);
